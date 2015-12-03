9356a2ad8bc03e2177fb13e4768b54af5ae9aadb
adesanto <aarondesanto@gmail.com>
2015-12-03 14:06:51 -0500

    messing with gulpsquatch

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
-var gulp = require("gulp");
-var stylus = require("gulp-stylus");
-var livereload = require("gulp-livereload");
+var gulp = require("gulp"),
+    stylus = require("gulp-stylus"),
+    browserSync = require("browser-sync").create();
 
-gulp.task("default", ["styles"] ,function() {
-  console.log("Default complete!");
+gulp.task("default", ["watch", "styles"] ,function() {
+  console.log("__Default complete!");
 });
 
 gulp.task("styles", function () {
   gulp.src("./css/styles.styl")
     .pipe(stylus())
-    .pipe(gulp.dest("./css"));
+    .pipe(gulp.dest("./css"))
+    .pipe(browserSync.stream());
+  console.log("__got styles.styl");
+  console.log("__compiling styles.css");
+  console.log("__saved to ./css/styles.css");
+});
+
+gulp.task("watch", ["styles"], function() {
+  browserSync.init({
+    server: "."
+  });
+  gulp.watch("./css/styles.styl", ["styles"]);
+  gulp.watch("index.html").on("change", browserSync.reload);
 });
